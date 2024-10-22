7cc8a50d924f5790c6e4a34f9eff0931f020bd0e
ClydeEroor <95631245+ClydeEroor@users.noreply.github.com>
2024-10-22 18:20:19 +0300

    delete comment

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -10,92 +10,92 @@ jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('App Component', () => {
- test('renders loading state initially', async () => {
-  mockedAxios.get.mockReturnValue(new Promise(() => {})); // Оставить обещание нерешенным, имитируя длительную загрузку
-  await act(async () => {
-   render(
-    <MemoryRouter>
-     <SWRConfig value={{ dedupingInterval: 0, provider: () => new Map() }}>
-      <App />
-     </SWRConfig>
-    </MemoryRouter>,
-   );
+  test('renders loading state initially', async () => {
+    mockedAxios.get.mockReturnValue(new Promise(() => {}));
+    await act(async () => {
+      render(
+        <MemoryRouter>
+          <SWRConfig value={{ dedupingInterval: 0, provider: () => new Map() }}>
+            <App />
+          </SWRConfig>
+        </MemoryRouter>
+      );
+    });
+
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
   });
 
-  expect(screen.getByText('Loading...')).toBeInTheDocument();
- });
+  test('displays error message when fetching data fails', async () => {
+    const mockError = new Error('Failed to fetch mockHeroList');
+    mockedAxios.get.mockRejectedValueOnce(mockError);
 
- test('displays error message when fetching data fails', async () => {
-  const mockError = new Error('Failed to fetch mockHeroList');
-  mockedAxios.get.mockRejectedValueOnce(mockError);
+    await act(async () => {
+      render(
+        <MemoryRouter>
+          <SWRConfig value={{ dedupingInterval: 0, provider: () => new Map() }}>
+            <App />
+          </SWRConfig>
+        </MemoryRouter>
+      );
+    });
 
-  await act(async () => {
-   render(
-    <MemoryRouter>
-     <SWRConfig value={{ dedupingInterval: 0, provider: () => new Map() }}>
-      <App />
-     </SWRConfig>
-    </MemoryRouter>,
-   );
+    await waitFor(() => {
+      expect(screen.getByText('Error loading data')).toBeInTheDocument();
+    });
   });
 
-  await waitFor(() => {
-   expect(screen.getByText('Error loading data')).toBeInTheDocument();
-  });
- });
+  test('renders hero list correctly after data fetch', async () => {
+    mockedAxios.get.mockResolvedValueOnce({ data: mockHeroList });
 
- test('renders hero list correctly after data fetch', async () => {
-  mockedAxios.get.mockResolvedValueOnce({ data: mockHeroList });
+    await act(async () => {
+      render(
+        <MemoryRouter>
+          <SWRConfig value={{ dedupingInterval: 0, provider: () => new Map() }}>
+            <App />
+          </SWRConfig>
+        </MemoryRouter>
+      );
+    });
 
-  await act(async () => {
-   render(
-    <MemoryRouter>
-     <SWRConfig value={{ dedupingInterval: 0, provider: () => new Map() }}>
-      <App />
-     </SWRConfig>
-    </MemoryRouter>,
-   );
-  });
-
-  await waitFor(() => {
-   mockHeroList.results.forEach((hero) => {
-    expect(screen.getByText(hero.name)).toBeInTheDocument();
-   });
-  });
- });
-
- test('handles pagination correctly', async () => {
-  mockedAxios.get.mockResolvedValueOnce({ data: mockHeroList });
-
-  await act(async () => {
-   render(
-    <MemoryRouter>
-     <SWRConfig value={{ dedupingInterval: 0, provider: () => new Map() }}>
-      <App />
-     </SWRConfig>
-    </MemoryRouter>,
-   );
+    await waitFor(() => {
+      mockHeroList.results.forEach((hero) => {
+        expect(screen.getByText(hero.name)).toBeInTheDocument();
+      });
+    });
   });
 
-  await waitFor(() =>
-   expect(screen.getByText(mockHeroList.results[0].name)).toBeInTheDocument(),
-  );
+  test('handles pagination correctly', async () => {
+    mockedAxios.get.mockResolvedValueOnce({ data: mockHeroList });
 
-  const mockSecondPageData = {
-   count: 20,
-   results: [
-    { id: '11', name: 'Hero 11' },
-    { id: '12', name: 'Hero 12' },
-   ],
-  };
-  mockedAxios.get.mockResolvedValueOnce({ data: mockSecondPageData });
+    await act(async () => {
+      render(
+        <MemoryRouter>
+          <SWRConfig value={{ dedupingInterval: 0, provider: () => new Map() }}>
+            <App />
+          </SWRConfig>
+        </MemoryRouter>
+      );
+    });
 
-  const nextPageButton = screen.getByRole('button', { name: '2' });
-  fireEvent.click(nextPageButton);
+    await waitFor(() =>
+      expect(screen.getByText(mockHeroList.results[0].name)).toBeInTheDocument()
+    );
 
-  await waitFor(() => {
-   expect(screen.getByText('Hero 11')).toBeInTheDocument();
-   expect(screen.getByText('Hero 12')).toBeInTheDocument();
+    const mockSecondPageData = {
+      count: 20,
+      results: [
+        { id: '11', name: 'Hero 11' },
+        { id: '12', name: 'Hero 12' },
+      ],
+    };
+    mockedAxios.get.mockResolvedValueOnce({ data: mockSecondPageData });
+
+    const nextPageButton = screen.getByRole('button', { name: '2' });
+    fireEvent.click(nextPageButton);
+
+    await waitFor(() => {
+      expect(screen.getByText('Hero 11')).toBeInTheDocument();
+      expect(screen.getByText('Hero 12')).toBeInTheDocument();
+    });
   });
- });
 });
